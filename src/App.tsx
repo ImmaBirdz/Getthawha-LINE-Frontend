1557383255e43655e65ccf9168dfd058492f73e8
ImmaBirdz <nakeza1375@hotmail.com>
2025-09-07 19:57:28 +0700

    Create Register landing page

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Home from './components/Home'
+// import Register from './components/Register'
 // import Booking from './components/Booking'
 // import CouponModal from './components/CouponModal'
 
 function App() {
+  // const profile = false; // or true, depending on your logic
 
 return (
   <BrowserRouter>
@@ -12,9 +14,17 @@ return (
       {/* <Route path="/mybooking" element={<Booking />} /> */}
       {/* <Route path="/mybooking/:{booking-id}" element={<Modal />} /> */}
     </Routes>
+    // profile ? (
           {/* <Route path="/mycoupon/:couponId" element={<CouponModal />} /> */}
 
-  </BrowserRouter>
-);
+      // wait for main website api to check if user is registered
+    // ) : (
+    //   <BrowserRouter>
+    //     <Routes>
+    //       <Route path="/" element={<Register />} />
+    //     </Routes>
+    //   </BrowserRouter>
+    // )
+  );
 }
 export default App
