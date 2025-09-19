74d8a58b59bc1451004171b8e3dc53e92cfc3f69
ImmaBirdz <nakeza1375@hotmail.com>
2025-09-19 23:59:44 +0700

    test #4.3

--- src/context/ProfileContext.tsx
+++ src/context/ProfileContext.tsx
@@ -34,14 +34,14 @@ const ProfileContext = createContext<{
 });
 
 const ProfileProvider = (props: React.PropsWithChildren<{}>) => {
-    // const [profile, setProfile] = useState<Profile | null>(null)
-    const [profile, setProfile] = useState<Profile | null>({
-        userId: 'U1234567890abcdef1234567890abcdef',
-        displayName: 'John Doe',
-        pictureUrl: 'https://example.com/profile.jpg',
-        statusMessage: 'Hello, world!',
-        email: 'john.doe@example.com'
-    });
+    const [profile, setProfile] = useState<Profile | null>(null)
+    // const [profile, setProfile] = useState<Profile | null>({
+    //     userId: 'U1234567890abcdef1234567890abcdef',
+    //     displayName: 'John Doe',
+    //     pictureUrl: 'https://example.com/profile.jpg',
+    //     statusMessage: 'Hello, world!',
+    //     email: 'john.doe@example.com'
+    // });
     const [isLiffLoaded, setIsLiffLoaded] = useState(false);
     const [isLogin, setIsLogin] = useState(false);
 
