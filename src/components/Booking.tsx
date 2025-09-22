fb586ac97d296260f7b389f32010b7ab0e215b17
660615026Nutt <nutt_wee@cmu.ac.th>
2025-09-22 23:59:54 +0700

    delete assests

--- src/components/Booking.tsx
+++ src/components/Booking.tsx
@@ -8,9 +8,6 @@ import {
 import { getBookings } from '../services/backendApi';
 
 // import assets
-// import Calendar from '../assets/calendar.png';
-// import Clock from '../assets/clock.png';
-// import Location from '../assets/location.png';
 import Backtohomepage from '../assets/backhome.png';
 
 type Booking = {
