de284343414e4c2045af225a2f1f580a1cf036ee
660615026Nutt <nutt_wee@cmu.ac.th>
2025-09-23 00:56:44 +0700

    update delete here we go again

--- src/components/Booking.tsx
+++ src/components/Booking.tsx
@@ -139,8 +139,8 @@ function Booking() {
                     )}
                   </div>
 
-                  {/* Delete icon - bottom right corner */}
-                  <img src={bin} alt="Delete booking" onClick={() => handleDeleteBooking(booking.id)} className={`absolute bottom-1 right-1 w-8 h-8 cursor-pointer hover:scale-110 transition-transform duration-200 ${deleting === booking.id ? 'opacity-50 cursor-not-allowed' : 'hover:opacity-80'}`} />
+                  {/* Delete icon - center right */}
+                  <img src={bin} alt="Delete booking" onClick={() => handleDeleteBooking(booking.id)} className={`absolute top-1/2 right-4 transform -translate-y-1/2 w-8 h-8 cursor-pointer hover:scale-110 transition-transform duration-200 ${deleting === booking.id ? 'opacity-50 cursor-not-allowed' : 'hover:opacity-80'}`} />
                 </div>
               </li>
             ))}
