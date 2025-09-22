1ddf98f532ab71fdc0d38aa9ebf28a4558d5f160
660615026Nutt <nutt_wee@cmu.ac.th>
2025-09-23 00:39:57 +0700

    delete some trash

--- src/components/Booking.tsx
+++ src/components/Booking.tsx
@@ -143,13 +143,9 @@ function Booking() {
                   <button
                     onClick={() => handleDeleteBooking(booking.id)}
                     disabled={deleting === booking.id}
-                    className="absolute bottom-2 right-2 w-8 h-8 bg-red-100 hover:bg-red-200 rounded-lg flex items-center justify-center transition-colors disabled:opacity-50 disabled:cursor-not-allowed"
+                    className="absolute bottom-2 right-2 w-8 h-8 rounded-lg "
                   >
-                    <img 
-                      src={bin} 
-                      alt="Delete booking" 
-                      className="w-4 h-4"
-                    />
+                    <img src={bin} alt="Delete booking" className="w-4 h-4" />
                   </button>
                 </div>
               </li>
