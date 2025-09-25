04d28b6acdeb938758f0628e6ac0c3be4070717f
660615026Nutt <nutt_wee@cmu.ac.th>
2025-09-25 16:21:57 +0700

    fuck up show color

--- src/components/ViewBooking.tsx
+++ src/components/ViewBooking.tsx
@@ -128,7 +128,7 @@ function ViewBooking() {
           </div>
 
       {/* border line */}
-        <div className="mx-2 h-1 bg-[#DEC33A]"></div>
+        <div className="mx-2 h-1 !bg-[#DEC33A]"></div>
 
           {/* Dynamic Booking list from Backend API */}
           <ul className="flex flex-col gap-4 w-full px-4 list-none">
